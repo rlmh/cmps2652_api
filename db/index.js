32fd294c5876bcd4c38f8c50656dcbc798816dfd
= <lmherrera6@gmail.com>
2019-03-04 16:51:43 -0600

    fixed query

--- db/index.js
+++ db/index.js
@@ -1,10 +1,21 @@
 const db = require('../connector')
 
 const persons = () => {
-    return db.query('select * from person', (error, results) => {
-        if(error) 
-            return error;
-        return results[0].solution;
+    // return a promise
+    return new Promise((resolve, reject) => {
+        // attempt to connect to database
+        db.connect( error => {
+            if(error) // if there is an error reject
+                reject(error)
+            // if no error execute query
+            db.query('select * from person', (error, results) => {
+                // if there was an error executing query
+                if(error)
+                    reject(error)
+                // return results
+                resolve(results)
+            })
+        })
     })
 }
 
--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const http = require('http')
 const path = require('path')
 const { persons } = require('./db')
 
-let app = express()
+const app = express()
     .use(express.static(
         path.join(__dirname + '/static')
     ))
     .get('/', (request, response) => {
         response.send('test')
     })
-    .get('/person', (request, response) => {
-        console.log(persons())
-        response.send(persons())
+    .get('/persons', (request, response) => {
+        persons().then(data => {
+            response.send(data)
+        })
     })
 
 http.createServer(app)
